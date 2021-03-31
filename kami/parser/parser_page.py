171b5f60abf2d1dbedf45b56a932509d2afa3547
alix.chague@inria.fr <alix.chague@inria.fr>
2021-03-31 17:40:57 +0200

    final polishing after running basic tests

--- kami/parser/parser_page.py
+++ kami/parser/parser_page.py
@@ -18,45 +18,6 @@ from bs4 import BeautifulSoup
 from kami.kamutils._utils import (_report_log)
 
 
-def _get_page_content(page):
-    """Process PageParser input variable to obtain parsed PAGE XML tree
-
-    Example:
-    >>> _get_page_content("incorrect/path/to/unexisting/file.txt")
-    None
-
-    >>> type(_get_page_content("path/to/existing/file.xml"))
-    <class 'bs4.BeautifulSoup'>
-    >>> type(_get_page_content(BeautifulSoup('<example/>', 'xml')))
-    <class 'bs4.BeautifulSoup'>
-
-    :param page: either a path (str) either a parsed XML Tree
-    :type page: str | BeautifulSoup
-    :return: Parsed XML tree
-    :rtype: BeautifulSoup
-    """
-    page_content = None
-    if isinstance(page, type(BeautifulSoup())):
-        page_content = page
-    elif isinstance(page, str):
-        if page.endswith('.xml'):
-            _report_log("Pageparser expects PAGE XML content or a .xml file.", "W")
-        elif os.path.isfile(page):
-            try:
-                with open(page, 'r') as fh:
-                    file_content = fh.read()
-            except OSError as e:
-                _report_log("Something went wrong while parsing source file:", "W")
-                print(e)
-            else:
-                try:
-                    page_content = BeautifulSoup(file_content, 'xml')
-                except TypeError as e:
-                    _report_log("Something went wrong while parsing XML content:", "W")
-                    print(e)
-    return page_content
-
-
 def _clean_coords(baseline_tag) -> list:
     """Clean coordinates following Kraken's expectation
 
@@ -77,7 +38,8 @@ def _clean_coords(baseline_tag) -> list:
 class PageParser:
     """A PAGE XML Parser for KaMI.
 
-    Attibutes:
+    Attributes:
+        file_name       Source file name if there is one
         page_content    XML Tree parsed with BeautifulSoup
         _pcgts          True if XML Tree has a PcGts node
         bounds          List of bounds for Kraken, 1 bound per segment
@@ -89,6 +51,47 @@ class PageParser:
 
     PageParser.bounds[1] matches PageParser.transcriptions[1] and PageParser.pairs[1]
     """
+
+    def _get_page_content(self, page):
+        """Process PageParser input variable to obtain parsed PAGE XML tree
+
+        Example:
+        >>> _get_page_content("incorrect/path/to/unexisting/file.txt")
+        None
+
+        >>> type(_get_page_content("path/to/existing/file.xml"))
+        <class 'bs4.BeautifulSoup'>
+        >>> type(_get_page_content(BeautifulSoup('<example/>', 'xml')))
+        <class 'bs4.BeautifulSoup'>
+
+        :param page: either a path (str) either a parsed XML Tree
+        :type page: str | BeautifulSoup
+        :return: Parsed XML tree
+        :rtype: BeautifulSoup
+        """
+        if isinstance(page, type(BeautifulSoup())):
+            self.page_content = page
+        elif isinstance(page, str):
+            if os.path.isfile(page) and not page.endswith('.xml'):
+                _report_log("PageParser expects PAGE XML content or a .xml file.", "E")
+            elif os.path.isfile(page):
+                try:
+                    with open(page, 'r', encoding='utf8') as fh:
+                        file_content = fh.read()
+                except OSError as e:
+                    _report_log("Something went wrong while parsing source file:", "E")
+                    print(e)
+                else:
+                    try:
+                        self.page_content = BeautifulSoup(file_content, 'xml')
+                    except TypeError as e:
+                        _report_log("Something went wrong while parsing XML content:", "E")
+                        print(e)
+                    else:
+                        self.file_name = page
+            else:
+                _report_log(f"'{page}' does not exist.", "E")
+
     def _get_lines(self) -> list:
         """Get TextLine elements in PAGE XML Tree"""
         return [line for line in self.page_content.find_all("TextLine")]
@@ -133,13 +136,15 @@ class PageParser:
         :param page: either a path (str) either a parsed XML Tree
         :type page: str or BeautifulSoup
         """
-        self.page_content = _get_page_content(page)
+        self.file_name = None
+        self.page_content = None
         self._pcgts = None
         self.bounds = None
         self.transcriptions = None
-        self.paris = None
+        self.pairs = None
         self.source_image = None
 
+        self._get_page_content(page)
         # proceed only if something was parsed
         if not self.page_content:
             _report_log("PageParser can't proceed: verify your input. Created an empty object.", "W")
@@ -159,9 +164,11 @@ class PageParser:
                 try:
                     self._get_bounds_and_transcriptions()
                 except Exception as e:
-                    _report_log("Something wen wrong while parsing TextLines", "E")
+                    _report_log("Something went wrong while parsing TextLines", "E")
                     print(e)
 
             else:
-                _report_log("Not a PAGE XML file", "E")
+                _report_log("Source is not a PAGE XML file", "E")
+                _report_log("PageParser can't proceed: verify your input. Created an empty object.", "W")
+
 
