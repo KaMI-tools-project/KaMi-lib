eb5863bfa9815bcd21db17e593d3346c7e26afcd
lterriel <lucas.terriel@inria.fr>
2022-01-10 16:58:32 +0100

    new version 0.1.2 done

--- kami/__init__.py
+++ kami/__init__.py
@@ -2,8 +2,8 @@
 
 __name__ = "kami"
 __title__ = "kamilib"
-# PiPy == "0.1.1"
-# testPiPy == "0.1.1b27"
-__version__ = "0.1.1b45"
+# PiPy == "0.1.2a"
+# testPiPy == "0.1.1b45"
+__version__ = "0.1.2"
 __licence__ = "MIT"
 __doc__ = "HTR / OCR models evaluation agnostic Python package, originally based on the Kraken transcription system."
